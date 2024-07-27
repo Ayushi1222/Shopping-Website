278b35ce1a0c6c3789108510affed25999e9ad83
Ayushi katroliya <ayushikatroliya17h@gmail.com>
2024-07-27 14:17:42 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,6 +9,7 @@ function Navbar({ productCount }) {
 
   return (
     <nav className="bg-white shadow-xl">
+      <di>Hello</di>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-24">
           <div className="flex-shrink-0 flex items-center">
